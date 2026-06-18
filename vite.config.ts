90357e0c0acb8850761fd65ffc542f06bbb88951
thiagomaieski <kalebigames@gmail.com>
2026-06-18 20:54:11 -0300

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/site-mpd/', // Necessário para implantar no GitHub Pages (/site-mpd/)
 })
+
